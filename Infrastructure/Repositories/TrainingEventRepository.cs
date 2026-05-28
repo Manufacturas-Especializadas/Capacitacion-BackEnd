b02285880a60db11c924a114eb5fa566ae77378c
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-28 10:36:47 -0600

    feat(Repository): en esta interfaz se usa .Include() de Entity Framework para hidratar nuestro objeto completo y resolver el problema de la matriz de evaluaciones.

---
+++ Infrastructure/Repositories/TrainingEventRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class TrainingEventRepository(ApplicationDbContext context) : GenericRepository<TrainingEvent>(context), ITrainingEventRepository
+    {
+        public async Task<TrainingEvent?> GetEventWithDetailAsync(int eventId)
+        {
+            return await _context.TrainingEvents
+                .Include(e => e.Room)
+                .Include(e => e.Topics.OrderBy(t => t.TrainingEvent))
+                .Include(e => e.Attendees)
+                    .ThenInclude(a => a.Employee)
+                .Include(e => e.Attendees)
+                    .ThenInclude(a => a.Evaluations)
+                .FirstOrDefaultAsync(e => e.Id == eventId);
+        }
+
+        public async Task<IReadOnlyList<TrainingEvent>> GetActiveEventsAsync()
+        {
+            return await _context.TrainingEvents
+                    .Where(e => e.Status != "Completado")
+                    .OrderBy(e => e.DateFrom)
+                    .AsNoTracking()
+                    .ToListAsync();
+        }
+    }
+}
