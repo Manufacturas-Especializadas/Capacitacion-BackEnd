6b3fc46f42d822aa24b8295a7ac91fc5117d7eda
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-28 10:30:56 -0600

    feat(Interface): Interfaz TrainingEvent con extension de la interfaz generica

---
+++ Domain/Interfaces/ITrainingEventRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface ITrainingEventRepository : IGenericRepository<TrainingEvent>
+    {
+        Task<TrainingEvent?> GetEventWithDetailAsync(int eventId);
+
+        Task<IReadOnlyList<TrainingEvent>> GetActiveEventsAsync();
+    }
+}
