7bc9645b72a8959c9f0f73c90734b5c8dffe804f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-28 10:39:23 -0600

    feat(dependency): en este archivo registramos las dependencias

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("Connection")));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ITrainingEventRepository, TrainingEventRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
