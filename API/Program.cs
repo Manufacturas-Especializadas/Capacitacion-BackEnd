a9db6aada3805535458c7e91cf83a445c9429a16
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-28 14:49:59 -0600

    feat(program): configuracion de swagger

--- API/Program.cs
+++ API/Program.cs
@@ -1,20 +1,36 @@
+using Application;
+using Infrastructure;
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.AddApplication();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Capacitacion MESA API",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Capacitacion MESA API v1");
+});
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+// Redirección automática a la interfaz gráfica
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.Run();
