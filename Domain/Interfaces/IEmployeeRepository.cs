443fbefec8184a275410704b7da2948f0ab5d2c6
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-28 12:11:30 -0600

    feat(IRepository): interfaz para obtener el numero de nomina de empleado

---
+++ Domain/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IEmployeeRepository : IGenericRepository<Employee>
+    {
+        Task<Employee?> GetByEmployeeNumberAsync(string employeeNumber);
+    }
+}
