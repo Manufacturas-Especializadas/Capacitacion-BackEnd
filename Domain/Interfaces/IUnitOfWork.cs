d23e912e02a60370b7138f73977ac612bffdcb1a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-28 12:14:35 -0600

    feat(IRepositoyr): integracion de los metodo de las nuevas interfaces

--- Domain/Interfaces/IUnitOfWork.cs
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -1,8 +1,15 @@
-﻿namespace Domain.Interfaces
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
         ITrainingEventRepository TrainingEvents { get; }
+        IEmployeeRepository Employees { get; }
+        IProductionLineRepository ProductionLines { get; }
+
+        IGenericRepository<EventAttendee> EventAttendees { get; }
+        IGenericRepository<TopicEvaluation> TopicEvaluations { get; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
