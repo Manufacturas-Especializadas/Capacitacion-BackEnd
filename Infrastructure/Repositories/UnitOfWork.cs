849574265aeab66bb112e04e8247cb15d7708e53
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-28 10:37:29 -0600

    feat(Repository): en este repositorio empaquetamos todo en la unidad de trabajo

---
+++ Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Persistence;
+
+namespace Infrastructure.Repositories
+{
+    public class UnitOfWork(ApplicationDbContext context, ITrainingEventRepository trainingEventRepository) : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public ITrainingEventRepository TrainingEvents { get; } = trainingEventRepository;
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
