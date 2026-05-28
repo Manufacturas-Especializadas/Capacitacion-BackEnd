5acad39dc909aea2c4ccfddf04f3af6c556847af
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-28 12:15:53 -0600

    feat(Repository): metodo para obtener la nomina del empleado

---
+++ Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class EmployeeRepository(ApplicationDbContext context) : GenericRepository<Employee>(context), IEmployeeRepository
+    {
+        public async Task<Employee?> GetByEmployeeNumberAsync(string employeeNumber)
+        {
+            return await _context.Employees
+                .FirstOrDefaultAsync(e => e.EmployeeNumber == employeeNumber);
+        }
+    }
+}
