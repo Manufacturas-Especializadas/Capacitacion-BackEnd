587ba05d460eec7b4cbe3ce16d478c86076529c7
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-28 12:12:32 -0600

    feat(IRepository): interfaz para obtener el nombre de la linea

---
+++ Domain/Interfaces/IProductionLineRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IProductionLineRepository : IGenericRepository<ProductionLine>
+    {
+        Task<ProductionLine?> GetByNameAsync(string lineName);
+    }
+}
