8b7d2546d043765f49d57e98852198561cec18cc
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-28 09:56:39 -0600

    feat(configuration): mapeo de propiedades de los modelos del DbContext

---
+++ Infrastructure/Persistence/Configurations/EventAttendeeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class EventAttendeeConfiguration : IEntityTypeConfiguration<EventAttendee>
+    {
+        public void Configure(EntityTypeBuilder<EventAttendee> builder)
+        {
+            builder.HasKey(ea => ea.Id);
+
+            builder.HasIndex(ea => new { ea.EventId, ea.EmployeeId }).IsUnique();
+
+            builder.Property(ea => ea.ParticipantSignatureUrl).HasMaxLength(1000);
+
+            builder.HasOne(ea => ea.TrainingEvent)
+                   .WithMany(te => te.Attendees)
+                   .HasForeignKey(ea => ea.EventId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(ea => ea.Employee)
+                   .WithMany(e => e.EventAttendances)
+                   .HasForeignKey(ea => ea.EmployeeId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
