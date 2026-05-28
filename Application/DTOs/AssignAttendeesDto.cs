e56058af9866cce35d518aee4b416ee4a14d9481
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-28 12:09:10 -0600

    feat(DTO): dto para asignar asistentes

---
+++ Application/DTOs/AssignAttendeesDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs
+{
+    public class AssignAttendeesDto
+    {
+        public int EventId { get; set; }
+
+        public List<AttendeeRowDto> Attendees { get; set; } = new();
+    }
+}
