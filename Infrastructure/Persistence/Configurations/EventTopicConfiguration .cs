0785b4100b5bcb290bee9f2dc253b2e64eb5d08a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-29 09:03:27 -0600

    feat(configuration): mapeo de la entidad EventTopic

---
+++ Infrastructure/Persistence/Configurations/EventTopicConfiguration .cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class EventTopicConfiguration : IEntityTypeConfiguration<EventTopic>
+    {
+        public void Configure(EntityTypeBuilder<EventTopic> builder)
+        {
+            builder.HasKey(et => et.Id);
+
+            builder.Property(et => et.TopicName).HasMaxLength(100).IsRequired();
+            builder.HasOne(et => et.TrainingEvent)
+                    .WithMany(te => te.Topics)
+                    .HasForeignKey(et => et.EventId)
+                    .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
