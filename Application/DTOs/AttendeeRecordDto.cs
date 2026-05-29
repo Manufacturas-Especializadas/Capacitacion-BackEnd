ee73bb6c8be09dc8e1a716d85f112e78b5370854
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-29 09:01:12 -0600

    feat(DTO): Nuevos Dtos

---
+++ Application/DTOs/AttendeeRecordDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.DTOs
+{
+    public class AttendeeRecordDto
+    {
+        public int EmployeeId { get; set; }
+
+        public string? Signature { get; set; }
+
+        public List<EvaluationUpdateDto> Evaluations { get; set; } = new();
+    }
+}
---
+++ Application/DTOs/SaveAttendanceDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.DTOs
+{
+    public class SaveAttendanceDto
+    {
+        public int EventId { get; set; }
+
+        public string? Comments { get; set; }
+
+        public string? InstructorSignature { get; set; }
+
+        public List<AttendeeRecordDto> EmployeeRecords { get; set; } = new();
+    }
+}
