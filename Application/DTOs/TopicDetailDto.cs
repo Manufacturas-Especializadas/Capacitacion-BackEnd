9c9fc534b268db8de4d3b28c9d1f736de3c50e8c
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-04 08:40:14 -0600

    feat(DTO): dto que obtiene las fechas y horarios indivuales

---
+++ Application/DTOs/TopicDetailDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.DTOs
+{
+    public class TopicDetailDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Date { get; set; } = string.Empty;
+
+        public string StartTime { get; set; } = string.Empty;
+
+        public string EndTime { get; set; } = string.Empty;
+    }
+}
