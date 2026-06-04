8a7633a18d9dfad609d7779d95bce5089ff173c5
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-04 08:30:42 -0600

    feat(DTO): dto para guardar las fechas y horas de forma individual para cada tema

---
+++ Application/DTOs/CreateTopicDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.DTOs
+{
+    public class CreateTopicDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime Date { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan EndTime { get; set; }
+    }
+}
