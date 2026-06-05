3e01e439564050d4c5c689d40cd9beec3fbb5c8b
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-05 10:35:12 -0600

    feat(controller): endpoint que registra los datos de la checklist de soldadores

---
+++ API/Controllers/WelderEvaluationsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Application.Features.WelderEvaluations.Commands;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WelderEvaluationsController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create([FromForm] CreateWelderEvaluationCommand commnad)
+        {
+            var evaluationId = await _mediator.Send(commnad);
+
+            return Ok(new
+            {
+                Id = evaluationId,
+                Message = "Evaluacion guardada exitosamente"
+            });
+        }
+    }
+}
