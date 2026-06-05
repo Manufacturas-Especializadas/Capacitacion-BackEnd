ff456cb5a9f15de8de3ad9853f929f17ad29ff76
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-05 10:54:33 -0600

    feat(DTO): dtos para obtener los detalles y las evaluaciones en forma de lista

---
+++ Application/DTOs/WelderEvaluationDetailDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.DTOs
+{
+    public class WelderEvaluationDetailDto
+    {
+        public int Id { get; set; }
+
+        public string EmployeeNumber { get; set; } = string.Empty;
+
+        public string EmployeeName { get; set; } = string.Empty;
+
+        public DateTime EvaluationDate { get; set; }
+
+        public decimal FinalAverage { get; set; }
+
+        public string MasteryLevel { get; set; } = string.Empty;
+    }
+}
---
+++ Application/DTOs/WelderEvaluationListDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.DTOs
+{
+    public class WelderEvaluationListDto
+    {
+        public int Id { get; set; }
+
+        public string EmployeeName { get; set; } = string.Empty;
+
+        public string EmployeeNumber { get; set; } = string.Empty;
+
+        public DateTime EvaluationDate { get; set; }
+
+        public decimal? FinalAverage { get; set; }
+
+        public string MasteryLevel { get; set; } = string.Empty;
+    }
+}
