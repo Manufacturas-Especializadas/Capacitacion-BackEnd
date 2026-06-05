59fa8ab5de0072fd4a5f9fd623d2ee76bee54f95
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-05 10:51:25 -0600

    feat(controlle): endpoint para obtener por Id

--- API/Controllers/WelderEvaluationsController.cs
+++ API/Controllers/WelderEvaluationsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MediatR;
 using Application.Features.WelderEvaluations.Commands;
+using Application.Features.TrainingEvents.Queries;
 
 namespace API.Controllers
 {
@@ -9,6 +10,12 @@ namespace API.Controllers
     public class WelderEvaluationsController(IMediator mediator) : ControllerBase
     {
         private readonly IMediator _mediator = mediator;
+
+        [HttpGet("getById/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _mediator.Send(new GetWelderEvaluationByIdQuery(id)));
+        }
 
         [HttpPost("create")]
         public async Task<IActionResult> Create([FromForm] CreateWelderEvaluationCommand commnad)
