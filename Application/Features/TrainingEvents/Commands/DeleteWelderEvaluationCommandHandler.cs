7fed1d9c1dceb38e55f1c31592a0fc81f1f0dc72
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-08 12:56:15 -0600

    feat(command): metodo para eliminar

---
+++ Application/Features/TrainingEvents/Commands/DeleteWelderEvaluationCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Features.TrainingEvents.Commands
+{
+    public record DeleteWelderEvaluationCommand(int Id) : IRequest<Unit>;
+
+    public class DeleteWelderEvaluationCommandHandler(IUnitOfWork unitOfWork) : IRequestHandler<DeleteWelderEvaluationCommand, Unit>
+    {
+        public async Task<Unit> Handle(DeleteWelderEvaluationCommand request, CancellationToken cancellationToken)
+        {
+            var eval = await unitOfWork.WelderEvaluations.GetByIdAsync(request.Id);
+            if (eval == null) throw new Exception("No encontrado");
+
+            unitOfWork.WelderEvaluations.Delete(eval);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
