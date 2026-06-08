60e60aafc3c971e52b78e4724de2f911579fe5c9
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-08 12:59:41 -0600

    feat(repository): metodos para obtener por id y obtener por llaves foraneas

---
+++ Infrastructure/Repositories/WelderEvaluationRepository.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class WelderEvaluationRepository(ApplicationDbContext context)
+            : GenericRepository<WelderEvaluation>(context), IWelderEvaluationRepository
+    {
+        public async Task<List<WelderEvaluation>> GetAllWithRelationsAsync()
+        {
+            return await _context.WelderEvaluations
+                .Include(w => w.Employee)
+                .OrderByDescending(w => w.EvaluationDate)
+                .ToListAsync();
+        }
+
+        public async Task<WelderEvaluation?> GetByIdWithRelationsAsync(int id)
+        {
+            return await _context.WelderEvaluations
+                .Include(w => w.Employee)
+                .Include(w => w.PracticalAnswers)
+                .Include(w => w.UnionAnswers)
+                .FirstOrDefaultAsync(w => w.Id == id);
+        }
+    }
+}
