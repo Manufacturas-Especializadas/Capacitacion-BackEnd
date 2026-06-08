4854275c177bd7332b2ad662b2aaa58edfe00702
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-08 12:53:35 -0600

    feat(Queyr): metodo para obtener lista de evaluaciones

---
+++ Application/Features/TrainingEvents/Queries/GetAllWelderEvaluationsQuery.cs
@@ -0,0 +1,27 @@
+﻿using Application.DTOs;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Features.TrainingEvents.Queries
+{
+    public class GetAllWelderEvaluationsQuery() : IRequest<List<WelderEvaluationListDto>>;
+
+    public class GetAllWelderEvaluationsQueryHandler(IUnitOfWork unitOfWork)
+        : IRequestHandler<GetAllWelderEvaluationsQuery, List<WelderEvaluationListDto>>
+    {
+        public async Task<List<WelderEvaluationListDto>> Handle(GetAllWelderEvaluationsQuery request, CancellationToken cancellationToken)
+        {
+            var list = await unitOfWork.WelderEvaluations.GetAllWithRelationsAsync();
+
+            return list.Select(e => new WelderEvaluationListDto
+            {
+                Id = e.Id,
+                EmployeeNumber = e.Employee?.EmployeeNumber ?? "N/A",
+                EmployeeName = e.Employee?.Name ?? "Sin nombre",
+                EvaluationDate = e.EvaluationDate,
+                FinalAverage = e.FinalAverage,
+                MasteryLevel = e.MasteryLevel
+            }).ToList();
+        }
+    }
+}
