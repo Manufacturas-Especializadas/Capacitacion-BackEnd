2c5db5e39449071ad7d60d6f2d7b65f753b2a29c
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-29 12:20:33 -0600

    feat(service): metodos para subir y eliminar archivos blob

--- Infrastructure/Services/BlobStorageService.cs
+++ Infrastructure/Services/BlobStorageService.cs
@@ -88,6 +88,46 @@ namespace Infrastructure.Services
             return blobClient.Uri.ToString();
         }
 
+        public async Task<string> UploadFileTrainingReportAsync(IFormFile file, string fileName)
+        {
+            if (file == null || file.Length == 0) return string.Empty;
+
+            var blobServiceClient = new BlobServiceClient(_connectionString);
+            var blobContainerClient = blobServiceClient.GetBlobContainerClient(_containerNameTrainingReports);
+
+            await blobContainerClient.CreateIfNotExistsAsync(PublicAccessType.Blob);
+
+            var uniqueFileName = $"{Guid.NewGuid()}-{fileName}";
+            var blobClient = blobContainerClient.GetBlobClient(uniqueFileName);
+
+            using var stream = file.OpenReadStream();
+            var blobHttpHeaders = new BlobHttpHeaders { ContentType = file.ContentType ?? "image/png" };
+
+            await blobClient.UploadAsync(stream, new BlobUploadOptions { HttpHeaders = blobHttpHeaders });
+
+            return blobClient.Uri.ToString();
+        }
+
+        public async Task DeleteFileTrainingReportAsync(string fileUrl)
+        {
+            if (string.IsNullOrWhiteSpace(fileUrl)) return;
+            try
+            {
+                var uri = new Uri(fileUrl);
+                var blobName = uri.Segments.Last();
+
+                var blobServiceClient = new BlobServiceClient(_connectionString);
+                var blobContainerClient = blobServiceClient.GetBlobContainerClient(_containerNameTrainingReports);
+                var blobClient = blobContainerClient.GetBlobClient(blobName);
+
+                await blobClient.DeleteIfExistsAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al borrar archivo del reporte: {ex.Message}");
+            }
+        }
+
         public async Task DeleteFileWeldersAsync(string fileUrl)
         {
             if (string.IsNullOrWhiteSpace(fileUrl)) return;
