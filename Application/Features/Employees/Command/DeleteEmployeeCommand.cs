0f5114e43eea3661395e3037c4738faf527e61e3
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-29 09:42:03 -0600

    feat(command): metodo delete employee

---
+++ Application/Features/Employees/Command/DeleteEmployeeCommand.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Features.Employees.Command
+{
+    public record DeleteEmployeeCommand(int Id) : IRequest<bool>;
+
+    public class DeleteEmployeeCommandHandler(IUnitOfWork unitOfWork) : IRequestHandler<DeleteEmployeeCommand, bool>
+    {
+        public async Task<bool> Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            var employee = await unitOfWork.Employees.GetByIdAsync(request.Id);
+
+            if (employee == null) return false;
+
+            unitOfWork.Employees.Delete(employee);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
