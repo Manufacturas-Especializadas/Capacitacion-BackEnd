2b02b915604f6d2ee4c153807056627880e391db
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-30 07:37:35 -0600

    feat(command): metodo para el registro de temas de entrenamientos

---
+++ Application/Features/TrainingTopics/Commands/CreateTrainingTopicCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Interfaces;
+using Application.DTOs;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.TrainingTopics.Commands
+{
+    public record CreateTrainingTopicCommand(CreateTrainingTopicDto Data) : IRequest<TrainingTopicDto>;
+
+    public class CreateTrainingTopicCommandHandler(IUnitOfWork unitOfWork) : IRequestHandler<CreateTrainingTopicCommand, TrainingTopicDto>
+    {
+        public async Task<TrainingTopicDto> Handle(CreateTrainingTopicCommand request, CancellationToken cancellationToken)
+        {
+            var topic = new TrainingTopic
+            {
+                TrainingType = request.Data.TrainingType.ToUpper(),
+                TopicCode = request.Data.TopicCode,
+                TopicName = request.Data.TopicName
+            };
+
+            await unitOfWork.TrainingTopics.AddAsync(topic);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return new TrainingTopicDto
+            {
+                Id = topic.Id,
+                TrainingType = topic.TrainingType,
+                TopicCode = topic.TopicCode,
+                TopicName = topic.TopicName
+            };
+        }
+    }
+}
