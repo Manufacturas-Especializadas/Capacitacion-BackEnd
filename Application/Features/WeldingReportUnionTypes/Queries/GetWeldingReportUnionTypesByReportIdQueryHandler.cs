8bfb58ad77d82688545c9bbfbad487b1596efdf9
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-30 09:57:55 -0600

    feat(query): metodo para obtener las uniones de soldadura por tipo

---
+++ Application/Features/WeldingReportUnionTypes/Queries/GetWeldingReportUnionTypesByReportIdQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Interfaces;
+using Application.DTOs;
+using MediatR;
+
+namespace Application.Features.WeldingReportUnionTypes.Queries
+{
+    public record GetWeldingReportUnionTypesByReportIdQuery(int ReportId) : IRequest<IEnumerable<WeldingReportUnionTypeDto>>;
+
+    public class GetWeldingReportUnionTypesByReportIdQueryHandler(IUnitOfWork unitOfWork) : IRequestHandler<GetWeldingReportUnionTypesByReportIdQuery, IEnumerable<WeldingReportUnionTypeDto>>
+    {
+        public async Task<IEnumerable<WeldingReportUnionTypeDto>> Handle(GetWeldingReportUnionTypesByReportIdQuery request, CancellationToken cancellationToken)
+        {
+            var allTypes = await unitOfWork.WeldingReportUnionTypes.GetAllAsync();
+            var filteredTypes = allTypes.Where(t => t.ReportId == request.ReportId);
+
+            return filteredTypes.Select(t => new WeldingReportUnionTypeDto
+            {
+                Id = t.Id,
+                ReportId = t.ReportId,
+                UnionTypeName = t.UnionName
+            });
+        }
+    }
+}
