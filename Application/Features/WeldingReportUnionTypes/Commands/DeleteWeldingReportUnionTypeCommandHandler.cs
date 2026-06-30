15c397e134bb709c2ad77bf3d7838077edb94789
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-30 09:54:19 -0600

    feat(command): delete de union de soldadura

---
+++ Application/Features/WeldingReportUnionTypes/Commands/DeleteWeldingReportUnionTypeCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Features.WeldingReportUnionTypes.Commands
+{
+    public record DeleteWeldingReportUnionTypeCommand(int Id) : IRequest<bool>;
+
+    public class DeleteWeldingReportUnionTypeCommandHandler(IUnitOfWork unitOfWork) : IRequestHandler<DeleteWeldingReportUnionTypeCommand, bool>
+    {
+        public async Task<bool> Handle(DeleteWeldingReportUnionTypeCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await unitOfWork.WeldingReportUnionTypes.GetByIdAsync(request.Id);
+
+            if (entity == null) return false;
+
+            unitOfWork.WeldingReportUnionTypes.Delete(entity);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
