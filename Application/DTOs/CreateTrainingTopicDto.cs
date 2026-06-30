6bbb8a6d9a65562dc10a5124bd48f50d59be893a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-30 07:37:07 -0600

    feat(DTO): dto para el registro de nuevos temas de entrenamientos

---
+++ Application/DTOs/CreateTrainingTopicDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.DTOs
+{
+    public class CreateTrainingTopicDto
+    {
+        public required string TrainingType { get; set; }
+
+        public required string TopicCode { get; set; } 
+
+        public required string TopicName { get; set; }
+    }
+}
