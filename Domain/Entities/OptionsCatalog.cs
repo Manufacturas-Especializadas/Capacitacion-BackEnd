a122e5ad953192600344756c55ae09749c70d2eb
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-20 11:05:17 -0600

    feat(entitie): navegacion

--- Domain/Entities/OptionsCatalog.cs
+++ Domain/Entities/OptionsCatalog.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
 
         public string OptionName { get; set; } = string.Empty;
+
+        public ICollection<QuestionOption> QuestionOptions { get; set; } = new List<QuestionOption>();
     }
 }
