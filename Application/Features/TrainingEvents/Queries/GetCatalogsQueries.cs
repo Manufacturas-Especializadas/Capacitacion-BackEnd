e123911969fd6127ded2585fdc679eb057ff7e68
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-20 10:56:30 -0600

    feat(query): metodo para obtener los tutores

--- Application/Features/TrainingEvents/Queries/GetCatalogsQueries.cs
+++ Application/Features/TrainingEvents/Queries/GetCatalogsQueries.cs
@@ -38,4 +38,21 @@ namespace Application.Features.TrainingEvents.Queries
             .ToList();
         }
     }
+
+    public record GetTutors(): IRequest<List<CatalogItemDto>>;
+
+    public class GetTutorsHandler(IUnitOfWork unitOfWork) : IRequestHandler<GetTutors, List<CatalogItemDto>>
+    {
+        public async Task<List<CatalogItemDto>> Handle(GetTutors request, CancellationToken cancellationToken)
+        {
+            var tutors = await unitOfWork.Tutors.GetAllAsync();
+            return tutors.Select(t => new CatalogItemDto
+            {
+                Id = t.Id,
+                Name = t.TutorName
+            })
+            .OrderBy(r => r.Id)
+            .ToList();
+        }
+    }
 }
