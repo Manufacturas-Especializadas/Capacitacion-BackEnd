0f10d73bad082b1e6f40be8d2e1ac3f5410dc378
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-20 11:27:10 -0600

    feat(configuration): mapeo de propiedades de las entidades del programa de tutoreo

---
+++ Infrastructure/Persistence/Configurations/QuestionOptionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class QuestionOptionConfiguration : IEntityTypeConfiguration<QuestionOption>
+    {
+        public void Configure(EntityTypeBuilder<QuestionOption> builder)
+        {
+            builder.HasKey(qo => new { qo.QuestionId, qo.OptionId });
+
+            builder.HasOne(qo => qo.Question)
+                .WithMany(q => q.QuestionOptions)
+                .HasForeignKey(qo => qo.QuestionId);
+
+            builder.HasOne(qo => qo.Option)
+                .WithMany(o => o.QuestionOptions)
+                .HasForeignKey(qo => qo.OptionId);
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Configurations/TutoringProgramConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class TutoringProgramConfiguration : IEntityTypeConfiguration<TutoringProgram>
+    {
+        public void Configure(EntityTypeBuilder<TutoringProgram> builder)
+        {
+            builder.HasKey(tp => tp.Id);
+
+            builder.Property(tp => tp.CollaboratorName)
+                .HasMaxLength(70)
+                .IsRequired();
+
+            builder.Property(tp => tp.Area)
+                .HasMaxLength(60)
+                .IsRequired();
+
+            builder.Property(tp => tp.CreatedDate)
+                .HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
