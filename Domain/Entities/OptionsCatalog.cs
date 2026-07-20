81460ef8eb2deb2e79c7f212e724a4b969084e97
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-20 07:54:12 -0600

    feat(entitie): entidad opciones

---
+++ Domain/Entities/OptionsCatalog.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public class OptionsCatalog
+    {
+        public int Id { get; set; }
+
+        public string OptionName { get; set; } = string.Empty;
+    }
+}
