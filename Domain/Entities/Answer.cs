9388faf52ef891f4e45cc436682cb129881fb911
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-20 11:24:07 -0600

    feat(entities): entidades para el programa de tutoreo

---
+++ Domain/Entities/Answer.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Entities
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+
+        public int TutoringProgramId { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public int? OptionId { get; set; }
+
+        public int? RatingValue { get; set; }
+
+        public string? TextValue { get; set; }
+
+        public TutoringProgram TutoringProgram { get; set; } = null!;
+
+        public Question Question { get; set; } = null!;
+
+        public OptionsCatalog? Option { get; set; }
+    }
+}
---
+++ Domain/Entities/Question.cs
@@ -0,0 +1,37 @@
+﻿using static System.Collections.Specialized.BitVector32;
+
+namespace Domain.Entities
+{
+    public class Question
+    {
+        public int Id { get; set; }
+
+        public int SectionId { get; set; }
+
+        public int QuestionTypeId { get; set; }
+
+        public string QuestionText { get; set; } = null!;
+
+        public int DisplayOrder { get; set; }
+
+        public bool IsRequired { get; set; }
+
+        public int? MaxRating { get; set; }
+
+        public int? ParentQuestionId { get; set; }
+
+        public int? ShowWhenOptionId { get; set; }
+
+        public Section Section { get; set; }
+
+        public QuestionType QuestionType { get; set; } = null!;
+
+        public Question? ParentQuestion { get; set; }
+
+        public OptionsCatalog? ShowWhenOption { get; set; }
+
+        public ICollection<Question> ChildQuestions { get; set; } = new List<Question>();
+
+        public ICollection<QuestionOption> QuestionOptions { get; set; } = new List<QuestionOption>();
+    }
+}
