4edc0d50310dacb08bcf2e4bb43ed9f40f78ad9b
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-21 07:58:35 -0600

    feat(controller): metodos CRUD - programa de tutoreo

---
+++ API/Controllers/TutoringProgramsController.cs
@@ -0,0 +1,52 @@
+﻿using Application.DTOs;
+using Application.Features.TrainingReports.Commands;
+using Application.Features.TutoringProgram.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TutoringProgramsController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("create")]
+        public async Task<IActionResult> Create([FromBody] CreateTutoringProgramDto request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var command = new CreateTutoringProgramCommand(request);
+            var result = await mediator.Send(command);
+
+            return Ok(result);
+        }
+
+        [HttpPut("update/{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateTutoringProgramDto request)
+        {
+            if (id != request.Id)
+                return BadRequest("El ID de la ruta no coincide con el ID del payload.");
+
+            var command = new UpdateTutoringProgramCommand(request);
+            var success = await mediator.Send(command);
+
+            if (!success)
+                return NotFound($"No se encontró el programa de tutoreo con el ID {id}.");
+
+            return NoContent();
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var command = new DeleteTutoringProgramCommand(id);
+            var success = await mediator.Send(command);
+
+            if (!success)
+                return NotFound($"No se encontró el programa de tutoreo con el ID {id}.");
+
+            return NoContent();
+        }
+    }
+}
