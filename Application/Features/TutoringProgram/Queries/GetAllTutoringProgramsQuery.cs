b6e406820b1f9b6e0fce3380c06c750123886342
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-22 11:27:22 -0600

    feat(query): metodo GET para obtener los programas

---
+++ Application/Features/TutoringProgram/Queries/GetAllTutoringProgramsQuery.cs
@@ -0,0 +1,36 @@
+﻿using Application.DTOs;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Features.TutoringProgram.Queries
+{
+    public record GetAllTutoringProgramsQuery() : IRequest<IEnumerable<TutoringProgramListDto>>;
+
+    public class GetAllTutoringProgramsQueryHandler(IUnitOfWork unitOfWork) : IRequestHandler<GetAllTutoringProgramsQuery, IEnumerable<TutoringProgramListDto>>
+    {
+        public async Task<IEnumerable<TutoringProgramListDto>> Handle(GetAllTutoringProgramsQuery request, CancellationToken cancellationToken)
+        {
+            var programs = await unitOfWork.TutoringPrograms.GetAllAsync();
+
+            if (programs == null || !programs.Any())
+            {
+                return Enumerable.Empty<TutoringProgramListDto>();
+            }
+
+            var result = programs.Select(p => new TutoringProgramListDto
+            {
+                Id = p.Id,
+                TutorId = p.TutorId,
+                CollaboratorName = p.CollaboratorName,
+                PayrollNumber = p.PayrollNumber,
+                Area = p.Area,
+                WeekId = p.WeekId,
+                CreatedDate = p.CreatedDate
+            })
+            .OrderByDescending(p => p.CreatedDate)
+            .ToList();
+
+            return result;
+        }
+    }
+}
