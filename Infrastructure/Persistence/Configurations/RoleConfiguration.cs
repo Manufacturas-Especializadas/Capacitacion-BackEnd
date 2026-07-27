d9872e347bc3a704721e5b729240ad82a1cdfd1d
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-27 12:21:32 -0600

    feat(Configuration): mepeo de propiedades Rol

---
+++ Infrastructure/Persistence/Configurations/RoleConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.ToTable("Roles");
+
+            builder.HasKey(r => r.Id);
+
+            builder.Property(r => r.RoleName)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            builder.HasIndex(r => r.RoleName)
+                .IsUnique();
+
+            builder.Property(r => r.Description)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        }
+    }
+}
