e32b0fed74de611aa000b13cfec42388afefcced
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-28 09:59:39 -0600

    feat(DTO): dtos auth

---
+++ Application/DTOs/AuthDTOs.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTOs
+{
+    public record LoginRequestDto(string PayrollNumber, string Password);
+
+    public record LoginResponseDto(string Token, string PayrollNumber, string Role);
+
+    public record CreateUserDto(string PayrollNumber, string Password, int RoleId);
+
+    public record UpdateUserDto(string PayrollNumber, int RoleId, bool IsActive);
+}
